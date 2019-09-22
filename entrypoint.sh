ff2720cb5766a6db0d007dfaa877fb0db4be4caa
Igor Golopolosov <igolopolosov@gmail.com>
2019-09-22 16:55:05 +0100

    [update] make repo name dynamic

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,23 +11,23 @@ COMMIT_NAME=`jq '.pusher.name' ${GITHUB_EVENT_PATH}`
 git config --global user.email "${COMMIT_EMAIL}"
 git config --global user.name "${COMMIT_NAME}"
 
-REPOSITORY_PATH="https://${GITHUB_ACCESS_TOKEN}@github.com/igolopolosov/igolopolosov.github.io.git"
+REPOSITORY_PATH="https://${GITHUB_ACCESS_TOKEN}@github.com/igolopolosov/$GITHUB_PAGES_REPO_NAME.git"
 
-echo "⭐️ Clone igolopolosov.github.io"
+echo "⭐️ Clone $GITHUB_PAGES_REPO_NAME"
 cd $GITHUB_WORKSPACE
 git clone $REPOSITORY_PATH
 
 echo "⭐️ Clean old files"
-cd ./igolopolosov.github.io
+cd ./$GITHUB_PAGES_REPO_NAME
 rm bundle*
 rm index.html
 
 echo "⭐️ Copying started"
 cd ../dist
-cp -R * ../igolopolosov.github.io
+cp -R * ../$GITHUB_PAGES_REPO_NAME
 
 echo "⭐️ Commit changes"
-cd ../igolopolosov.github.io
+cd ../$GITHUB_PAGES_REPO_NAME
 git add .
 git commit -m "Make auto release"
 
